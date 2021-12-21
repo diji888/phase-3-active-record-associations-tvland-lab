4cc5b3998dcd9c2ad80ba180af97e57cb59b7d75
Diana Jiang <jiang99diana@gmail.com>
2021-12-21 14:45:08 -0500

    commit tree 9943b5225e2a1d9d53d98d87b267a4288733d454

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,5 +1,9 @@
 class CreateActors < ActiveRecord::Migration[6.1]
   def change
-    # Your code here
+    create_table :actors do |t|
+      t.string :name
+      t.integer :actor_id
+      t.integer :show_id
+    end
   end
 end
